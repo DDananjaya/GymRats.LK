700df6f0a8273ec5129b84399e67d405507c32c1
M L D Dananjaya <dilishapadanjaya@gmail.com>
2026-04-06 11:09:03 +0530

    Fix formatting in db.js

--- api/db.js
+++ api/db.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
 const { createClient } = require("@libsql/client");
 
 const db = createClient({
-    url: process.env.TURSO_DATABASE_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN,
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
 module.exports = db;
